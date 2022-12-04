16744629bc265bdb46da3afcb990e6cc0f8ab3df
SaraDonzellini <saradonzie@gmail.com>
2022-12-04 13:42:52 +0100

    working  stopwatch

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -2,5 +2,24 @@ const tempo = document.getElementById('tempo');
 const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
+let conteggio = 0;
 
 
+start.addEventListener('click', function () {
+  const clock = setInterval(() => {
+    conteggio++
+    console.log(conteggio)
+    tempo.innerHTML = conteggio
+  }, 100);
+
+  pause.addEventListener('click', function () {
+    clearInterval(clock)
+    tempo.innerHTML = conteggio
+  })
+  reset.addEventListener('click', function () {
+    conteggio = 0
+    tempo.innerHTML = conteggio
+  })
+  
+  
+})
