1740e70bfe17f8c21e6986e758e7b42c87f6d212
SaraDonzellini <saradonzie@gmail.com>
2022-12-05 17:40:05 +0100

    removed start button bug

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -3,23 +3,29 @@ const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 let conteggio = 0;
-
+tempo.innerHTML = conteggio;
+let isClockStarted = false;
+let clock;
 
 start.addEventListener('click', function () {
-  const clock = setInterval(() => {
+  if (isClockStarted) {
+    return;
+  }
+    isClockStarted = true;
+    clock = setInterval(() => {
     conteggio++;
     console.log(conteggio);
     tempo.innerHTML = conteggio;
   }, 100);
 
-  pause.addEventListener('click', function () {
-    clearInterval(clock);
-    tempo.innerHTML = conteggio;
-  });
-  reset.addEventListener('click', function () {
-    conteggio = 0;
-    tempo.innerHTML = conteggio;
-  });
-  
-  
+});
+
+pause.addEventListener('click', function () {
+  clearInterval(clock);
+  tempo.innerHTML = conteggio;
+  isClockStarted = false;
+});
+reset.addEventListener('click', function () {
+  conteggio = 0;
+  tempo.innerHTML = conteggio;
 });
